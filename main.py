184d5155ea4afeb6810888f06a4eec9a4ff13c30
Allisson A. A. Garcia <allisson128@gmail.com>
2015-04-29 10:16:30 -0300

    funcao fitness

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 import snap
 
-# Cria grafo Nao direcionado:
-G1 = snap.TUNGraph.New()
+def ffitness (Graph):
+    return 2.*Graph.GetEdges()/((Graph.GetNodes()-1)*Graph.GetNodes())
 
-# Le Vertices e insere no grafo:
-with open('vert.dat') as file:
-    for line in file:
-        v = line.rstrip('\n')
-        G1.AddNode(int(v))
-        # print v 
+def main():
+    # Cria grafo Nao direcionado:
+    G1 = snap.TUNGraph.New()
 
-# Le e grava Arestas no grafo:
-lines = [line.strip() for line in open('edge.dat')]
-#print lines
-for e in lines:
-    dupla = e.split()
-#    print dupla
-    G1.AddEdge(int(dupla[0]), int(dupla[1]))
+    # Le Vertices e insere no grafo:
+    with open('vert.dat') as file:
+        for line in file:
+            v = line.rstrip('\n')
+            G1.AddNode(int(v))
+            # print v 
 
-print "G1: Nodes %d, Edges %d" % (G1.GetNodes(), G1.GetEdges())
+    # Le e grava Arestas no grafo:
+    lines = [line.strip() for line in open('edge.dat')]
+    #print lines
+    for e in lines:
+        dupla = e.split()
+        G1.AddEdge(int(dupla[0]), int(dupla[1]))
+        #print dupla
 
-# for NI in G1.Nodes():
-#     print "node id %d with out-degree %d and in-degree %d" % (NI.GetId(), NI.GetOutDeg(), NI.GetInDeg())
+
+    print "G1: Nodes %d, Edges %d" % (G1.GetNodes(), G1.GetEdges())
+    print ffitness(G1)
+
+    # for NI in G1.Nodes():
+    #     print "node id %d with degree %d" % (NI.GetId(), NI.GetDeg())
+    # for EI in G1.Edges():
+    #     print "edge (%d, %d)" % (EI.GetSrcNId(), EI.GetDstNId())
+    # Graph = snap.GenRndGnm(snap.PNGraph, 50, 500)
+    # nodes = snap.TIntV()
+    # for N in Graph.GetNI(0).GetOutEdges():
+    #     print N
+    #      nodes.Add(N)
+    #print nodes
+
+
+
+main()
+
